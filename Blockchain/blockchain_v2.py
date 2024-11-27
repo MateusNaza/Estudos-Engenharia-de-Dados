5e0c16aa7167d6d6f3c835248287839732ab3272
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2024-11-27 13:37:49 +0000

    UP: Metodos de prova, mineração e transações implementados

--- Blockchain/blockchain_v2.py
+++ Blockchain/blockchain_v2.py
@@ -36,19 +36,53 @@ class Blockchain:
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+    
 
+    # Aqui é tem a regra do hash e também é gerado o nonce
     def proof_of_work(self):
-        previous_block = json.dumps(self.chain[-1], indent=4)
+        last_block = json.dumps(self.chain[-1], sort_keys=True)
         nonce = 0
 
         while True:
-            guess = self.hash(previous_block)
-            if guess.startswith('000'):
-                return guess
+            merge_block_nonce = f"{last_block}{nonce}"
+            guess = self.hash(merge_block_nonce)
+
+            if guess.startswith('7777'):
+                return guess, nonce
             nonce += 1
 
+
+    def mine_block(self):
+        previous_hash, nonce = self.proof_of_work()
+        block = self.new_block(nonce, previous_hash)
+        return block
+    
+
+    def new_transaction(self, sender, recipient, amount):
+        # Aqui será gerado um dicionário para cada transação e colocado na lista de transações
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        # Retorna o índice do bloco onde será armazenada essa transação
+        return self.chain[0]
     
 # Criando uma instância da Blockchain
 bloco = Blockchain()
+bloco.new_transaction('Mateus', 'Larissa', 20)
+bloco.new_transaction('Mateus', 'Larissa', 1)
+bloco.new_transaction('Mateus', 'Larissa', 99)
+bloco_1 = bloco.mine_block()
+bloco.new_transaction('Bob', 'Larissa', 20)
+bloco.new_transaction('Mateus', 'Bob', 1)
+bloco.new_transaction('Mateus', 'Bob', 99)
+bloco_2 = bloco.mine_block()
+bloco_2 = bloco.mine_block()
+bloco_4 = bloco.mine_block()
 
-print(bloco.proof_of_work())
+
+print(json.dumps(bloco.chain, sort_keys=True))
+print(f'\n\n\n{bloco.hash('')}')
+
