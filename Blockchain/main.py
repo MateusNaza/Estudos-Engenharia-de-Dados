0971e700d59f61895451bf04e20389f017b63cfa
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2024-11-29 13:39:27 +0000

    UP Script de interação via fastAPI

---
+++ Blockchain/main.py
@@ -0,0 +1,36 @@
+import fastapi
+import blockchain_v2 as blockchain
+
+bc = blockchain.Blockchain()
+app = fastapi.FastAPI()
+
+
+# Endpoint de minerar blocos
+@app.post('/mine_block/')
+def mine_block():
+    block = bc.mine_block()
+    return block
+
+
+# Endpoint de efetuar transação
+@app.post('/new_transaction/')
+def new_transaction(sender, recipent, amount):
+    transaction = bc.new_transaction(sender, recipent, amount)
+    return transaction
+
+
+# Endpoint que retorna a blockchain completa
+@app.get('/chain/')
+def get_blockchain():
+    chain = bc.chain
+    return chain
+
+
+# Endpoint para verificar a lista de transações que ainda não foram para um bloco
+@app.get('/current_transactions/')
+def current_transactions():
+    transactions = bc.current_transactions
+    return transactions
+
+
+
