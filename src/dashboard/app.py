02c2652bebf97a43b13ae675db97779d07da7390
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-01-21 14:29:28 +0000

    feat: implementando dashboard

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -27,33 +27,36 @@ else:
 col1, col2, col3 = st.columns(3)
 
 
-# KPI 1 - Total de itens
+# --- KPI 1 - Total de itens ---
 total_itens = df_filter.shape[0]
 col1.metric(label='Total de itens', value=total_itens)
 
-# KPI 2 - Marcas analisadas
+# --- KPI 2 - Marcas analisadas ---
 unique_brands = df_filter['brand'].nunique()
 col2.metric(label='Total de Marcas', value=unique_brands)
 
-# KPI 3 - Preço médio atual
+# --- KPI 3 - Preço médio atual ---
 current_price_avg = df_filter['new_price'].mean().round(2)
 col3.metric(label='Preço médio atual (R$)', value=current_price_avg)
 
-st.subheader('Marcas mais encontradas')
 
-col1, col2 = st.columns([4,2])
+# --- Marcas mais encontradas na pesquisa e seus preços médios ---
+col1, col2 = st.columns([5,3])
 
-# Marcas mais encontradas na pesquisa
 top_brands = df_filter['brand'].value_counts().sort_values(ascending=False)
+brand_avg = df_filter.groupby('brand')['new_price'].mean().apply(lambda x: f"R$ {x:.2f}")
+
+col1.subheader('Quantidade de resultados por marca')
 col1.bar_chart(top_brands)
-col2.write(top_brands)
 
-# Melhores custo benefícios (Preço X Avaliação)
+col2.subheader('Preço médio por marca')
+col2.write(brand_avg)
 
 
+# --- Seleção de marcas ---
 selected_brand = st.selectbox('Selecione a Marca', df_filter['brand'].unique())
+
 df_filter_selected = df_filter[df_filter['brand'] == selected_brand]
 st.write(df_filter_selected)
 
-st.write(df_filter)
 
