12ba143f7173ae15050aa3af7e5664f289b08a37
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-01-31 18:37:59 +0000

    refactor: aadaptando código de geração de arquivos para o ambiente AWS

--- aws_utils.py
+++ aws_utils.py
@@ -2,6 +2,9 @@ import boto3
 from botocore.exceptions import ClientError
 import pandas as pd
 from io import StringIO
+import pyarrow as pa
+import pyarrow.parquet as pq
+from io import BytesIO
 
 session = boto3.Session()
 s3 = session.client('s3')
@@ -33,9 +36,9 @@ def process_csv_from_s3(bucket_name, s3_key):
         
         content = response['Body'].read().decode('utf-8')
         
-        df = pd.read_csv(StringIO(content), skiprows=2, sep=';', header=None)
-        
-        print(df.head())
+        df = pd.read_csv(StringIO(content), skiprows=2, sep=';', header=None, names=['City', 'Temperature'])
+
+        return df
         
     except Exception as e:
         print(f"Erro ao processar o arquivo CSV: {e}")
@@ -55,3 +58,22 @@ def delete_file_from_s3(bucket_name, s3_key):
         print(f"Arquivo '{s3_key}' deletado com sucesso do bucket '{bucket_name}'.")
     except ClientError as e:
         print(f"Erro ao deletar o arquivo: {e}")
+
+
+def save_parquet(lote_dados, bucket_name, s3_key):
+    """
+    Salva um lote de dados no formato Parquet diretamente no S3.
+    """
+    df = pd.DataFrame(lote_dados, columns=["station", "temperature"])
+    table = pa.Table.from_pandas(df)
+
+    # Converter o DataFrame para Parquet
+    parquet_buffer = BytesIO()
+    pq.write_table(table, parquet_buffer)
+
+    try:
+        # Enviar o Parquet para o S3
+        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=parquet_buffer.getvalue())
+        print(f"Dados enviados com sucesso para o bucket '{bucket_name}' com a chave '{s3_key}'.")
+    except Exception as e:
+        print(f"Erro ao enviar os dados para o S3: {e}")
--- create_csv.py
+++ create_csv.py
@@ -1,14 +1,20 @@
 import os
 import random
 import time
+import pyarrow as pa
+import pyarrow.parquet as pq
+from io import BytesIO
 
+import aws_utils as util
 
-def construir_lista_estacoes_meteorologicas():
+ 
+def construir_lista_estacoes_meteorologicas(bucket_name, s3_key_input):
     """
     Obtém os nomes das estações meteorológicas a partir de um arquivo e remove duplicatas.
     """
-    with open('./data/amostra_44k.csv', 'r', encoding="utf-8") as arquivo:
-        return list(set(linha.split(';')[0] for linha in arquivo if "#" not in linha))
+    df = util.process_csv_from_s3(bucket_name, s3_key_input)
+
+    return df['City'].unique()
 
 
 def converter_bytes(num):
@@ -28,36 +34,44 @@ def formatar_tempo_decorrido(segundos):
     minutos, segundos = divmod(segundos, 60)
     return f"{int(minutos)}m {int(segundos)}s" if minutos else f"{segundos:.2f}s"
 
-
-def gerar_dados_teste(num_registros):
+def gerar_dados_teste(num_registros, bucket_name, s3_key):
     """
-    Gera e escreve um arquivo com medições sintéticas de temperatura.
+    Gera e salva um arquivo Parquet com medições sintéticas de temperatura diretamente no S3.
     """
     inicio_tempo = time.time()
-    nomes_estacoes = construir_lista_estacoes_meteorologicas()
+    nomes_estacoes = construir_lista_estacoes_meteorologicas(bucket_name, s3_key_input)
     estacoes_10k_max = random.choices(nomes_estacoes, k=10_000)
-    arquivo_saida = f"./data/medicoes_{num_registros}.txt"
     tamanho_lote = 10_000  # Processamento em lotes
+    lote_dados = []
 
-    print(f"Criando {arquivo_saida}...")
+    print(f"Criando dados e enviando para o bucket '{bucket_name}'...")
 
     try:
-
-        dados = ""
         for _ in range(num_registros // tamanho_lote):
             lote = random.choices(estacoes_10k_max, k=tamanho_lote)
-            linhas = '\n'.join([f"{estacao};{random.uniform(-99.9, 99.9):.1f}" for estacao in lote])
-            dados += linhas + '\n'
+            lote_dados.extend([(estacao, round(random.uniform(-99.9, 99.9), 1)) for estacao in lote])
 
-        s3.put_object(Bucket=bucket_name, Key=s3_key, Body=dados.encode('utf-8'))
+            # Salvar em lotes para evitar consumo excessivo de memória
+            if len(lote_dados) >= 1_000_000:
+                util.save_parquet(lote_dados, bucket_name, f"{s3_key}/medicoes_{num_registros}_{time.time()}.parquet")
+                lote_dados = []  # Limpa a lista para o próximo lote
 
-        tamanho_arquivo = len(dados.encode('utf-8'))
-        print(f"Dados enviados para o bucket '{bucket_name}' com a chave '{s3_key}'.")
-        print(f"Tamanho final: {converter_bytes(tamanho_arquivo)}")
+        # Salvar qualquer dado restante
+        if lote_dados:
+            util.save_parquet(lote_dados, bucket_name, f"{s3_key}/medicoes_{num_registros}.parquet")
+
+        print(f"Dados enviados para o bucket '{bucket_name}' com a chave '{s3_key}/medicoes_{num_registros}.parquet'.")
         print(f"Tempo decorrido: {formatar_tempo_decorrido(time.time() - inicio_tempo)}")
+    
     except Exception as e:
-        print("Erro ao enviar os dados para o S3:")
+        print('Erro ao criar o arquivo Parquet:', e)
 
+num_registros = 1_000_000_000
+bucket_name = 'processando-1bilhao-linhas'
+s3_key_input = 'sample/amostra_44k.csv'
+s3_key = 'output'
+
+gerar_dados_teste(num_registros, bucket_name, s3_key)
 
 # if __name__ == "__main__":
 #     num_registros = 1_000_000_000  # Número de registros parametrizado
