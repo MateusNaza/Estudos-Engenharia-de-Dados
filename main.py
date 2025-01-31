b6da4e7514c79567c405c7e627ec5edd84218610
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-01-31 12:51:22 +0000

    feat: funções uteis de manipulação do s3

--- main.py
+++ main.py
@@ -1,48 +1,34 @@
 import boto3
 from botocore.exceptions import ClientError
+import pandas as pd
+from io import StringIO
+
 import create_csv
+import aws_utils as util
 
 '''
-Lembrando que antes de iniciar o código deve-se utilizar o comando bash aws aconfigure 
+Lembrando que antes de iniciar o código deve-se utilizar o comando bash aws configure 
 e preencher com os dados de usuário aws
 '''
 session = boto3.Session()
 s3 = session.client('s3')
 
-
-# ---- Funções ----
-
-def bucket_exists(bucket_name):
-    try:       
-        s3.head_bucket(Bucket=bucket_name)
-        return True
-    except ClientError:
-        return False
-
-
-def create_bucket(bucket_name, region=None):
-    try:
-        if region is None:
-            s3.create_bucket(Bucket=bucket_name)
-        else:
-            location = {'LocationConstraint': region}
-            s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration=location)
-
-        print(f"Bucket '{bucket_name}' criado com sucesso.")
-    except ClientError:
-        print(f'Falha na criação do bucket!')
-
-
-# ---- Variáveis ----
-
+# Variáveis
 bucket_name = 'processando-1bilhao-linhas'
 num_registros = 1_000_000_000
+file_name = 'data/amostra_44k.csv'
+s3_key = 'sample/amostra_44k.csv'
 
-# ---- Criação do Bucket ----
-if not bucket_exists(bucket_name):
-    create_bucket(bucket_name)
+# Criando Bucket se não existir
+if not util.bucket_exists(bucket_name):
+    util.create_bucket(bucket_name)
 
-# ---- Inserindo arquivos no Bucket ----
-create_csv.gerar_dados_teste(num_registros)
+# # Subindo arquivo de amostra para o bucket
+# util.upload_file_to_s3(file_name, bucket_name, s3_key)
+
+util.process_csv_from_s3(bucket_name, s3_key)
+
+# # ---- Inserindo arquivos no Bucket ----
+# create_csv.gerar_dados_teste(num_registros)
 
 
