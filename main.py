84dbf146ef200f6662cdf4da8395efc2760a10d9
Mateus Nazário <127886025+MateusNaza@users.noreply.github.com>
2025-02-03 21:11:13 +0000

    feat: iniciando processamentos no main.py

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
+import duckdb
+import time
 import boto3
-from botocore.exceptions import ClientError
-import pandas as pd
-from io import StringIO
+from io import BytesIO
 
-import create_csv
-import aws_utils as util
-
-'''
-Lembrando que antes de iniciar o código deve-se utilizar o comando bash aws configure 
-e preencher com os dados de usuário aws
-'''
 session = boto3.Session()
 s3 = session.client('s3')
 
-# Variáveis
-bucket_name = 'processando-1bilhao-linhas'
-num_registros = 1_000_000_000
-file_name = 'data/amostra_44k.csv'
-s3_key = 'sample/amostra_44k.csv'
+def read_parquet_from_s3(bucket, prefix):
+    response = s3.list_objects_v2(Bucket=bucket, Prefix=prefix)
+    arquivos = response.get('Contents', [])
+    
+    dfs = []
+    for arquivo in arquivos:
+        s3_key = arquivo['Key']
+        response = s3.get_object(Bucket=bucket, Key=s3_key)
+        parquet_buffer = BytesIO(response['Body'].read())
+        df = duckdb.read_parquet(parquet_buffer)
+        dfs.append(df)
+    
+    combined_df = duckdb.concat(dfs)
+    return combined_df
 
-# Criando Bucket se não existir
-if not util.bucket_exists(bucket_name):
-    util.create_bucket(bucket_name)
+def create_duckdb():
+    df = read_parquet_from_s3('processando-1bilhao-linhas', 'output/')
+    result = duckdb.sql("""
+        SELECT station,
+            MIN(temperature) AS min_temperature,
+            CAST(AVG(temperature) AS DECIMAL(3,1)) AS mean_temperature,
+            MAX(temperature) AS max_temperature
+        FROM df
+        GROUP BY station
+        ORDER BY station
+    """)
+    result.show()
 
-# # Subindo arquivo de amostra para o bucket
-# util.upload_file_to_s3(file_name, bucket_name, s3_key)
-
-util.process_csv_from_s3(bucket_name, s3_key)
-
-# # ---- Inserindo arquivos no Bucket ----
-# create_csv.gerar_dados_teste(num_registros)
-
-
+if __name__ == "__main__":
+    start_time = time.time()
+    create_duckdb()
+    took = time.time() - start_time
+    print(f"DuckDB Took: {took:.2f} sec")
